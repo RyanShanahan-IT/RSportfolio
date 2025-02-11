0f143156dc0ada3f3e4fa21720da2299b3025ff3
RyanShanahan-IT <rshan@outlook.ie>
2025-02-11 20:01:47 +0000

    Added Typewritter effect

--- script.js
+++ script.js
@@ -9,8 +9,20 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
     });
   });
 });
+/* Typewriter Effect for Text*/
+
+let i = 0;
+let text = "Welcome to My Portfolio site! My Name is Ryan Shanahan";
+function typeWriter() {
+    if (i < text.length) {
+        document.getElementById("typewriter").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, 100);
+    }
+}
+window.onload = typeWriter;
 
 // Contact button alert
 document.querySelector('#contact').addEventListener('click', () => {
-  alert('Thank you for visiting! Feel free to reach out.');
+  alert('Thank you for visiting! Feel free to reach out to any of my socials listed below!');
 });
